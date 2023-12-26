bf06859b20bcd23585cb40b30a5b0f88e45f9193
filizhelvaci <filizhelvaci82@gmail.com>
2023-12-26 09:11:37 +0300

    Main.java eklendi

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Kalitim;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Akademisyen a=new Akademisyen("Filiz Helvaci","flzhlvc@gmail.com","05469512919","Bilgisayar Müh.","Öğrenci Danışmanlığı","C");
+     a.getAdSoyad();
+   
+	}
+
+}
